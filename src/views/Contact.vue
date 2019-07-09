6ec2dd8687af73dcce267f00170f4c8bc573ac6a
Mikael Samvelian <mikael.samvelian@gmail.com>
2019-07-09 19:48:33 -0400

    Added map in contacts

---
+++ src/views/Contact.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
+             data-projection="EPSG:4326" style="height: 400px">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+
+      <vl-geoloc @update:position="geolocPosition = $event">
+        <template slot-scope="geoloc">
+          <vl-feature v-if="geoloc.position" id="position-feature">
+            <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+            <vl-style-box>
+              <vl-style-icon src="_media/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+            </vl-style-box>
+          </vl-feature>
+        </template>
+      </vl-geoloc>
+
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+    <div style="padding: 20px">
+      Zoom: {{ zoom }}<br>
+      Center: {{ center }}<br>
+      Rotation: {{ rotation }}<br>
+      My geolocation: {{ geolocPosition }}
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+  export default {
+    name: 'contact',
+    components: {
+    },
+    data () {
+      return { 
+        zoom: 14,
+        center: [-73.56670039591228, 45.49822605451584 ],
+        rotation: 0,
+        geolocPosition: undefined,
+      }
+    },
+  }
+</script>
